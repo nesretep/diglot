026e57b8d2bb0b46d1729f19f9419fceb51cf673
Kristian Petersen <nesretep.naitsirk@gmail.com>
2017-11-11 10:44:12 -0700

    Started skeleton for python REST API
    This still needs tons of work.  I am still trying to get an understanding of just how this works.

---
+++ python_api.py
@@ -0,0 +1,32 @@
+#! usr/bin/env python
+
+import requests
+import mysql.connector as mariadb
+
+
+def get_chapter(chapter_id):
+	pass
+
+
+def flip_words(not_sure_what_should_go_here):
+	pass
+
+
+def flip_one_word(position):
+	pass
+
+
+def at_critical_point():
+	pass
+
+
+def create_session_table(username):
+	pass
+
+
+def destroy_session_table(username):
+	pass
+
+
+def _url(path):
+    return 'https://example.com' + path
