f6a0654d6b0ac131e5aa45aba49d77c790721288
Kristian Petersen <nesretep.naitsirk@gmail.com>
2017-11-18 09:57:19 -0700

    Wrote docstrings for api functions; modifed chunk.py to include rank and a tag

--- api/chunk.py
+++ api/chunk.py
@@ -9,18 +9,23 @@ class Chunk(object):
             text (str)        : the actual Book of Mormon text of the chunk
             masterpos (str)   : master position this Chunk is connected to
             flipped (bool)    : False represents not flipped, True represents flipped
+            tag (dict)        : dict holding the boolean value for each of the different tag types
+            rank (int)        : the number representing the difficulty of the Chunk
             pos (int)         : the in-verse natural position of the Chunk
             verse (int)       : the verse the Chunk is in
             chapter (int)     : the chapter the Chunk is in
             book (int)        : a numeric representation of the book the Chunk is in with 1 Nephi being 1 and so on
             lang (str)        : the two-letter ISO designation for the language the Chunk is in
     """
-    def __init__(self, uid, text, masterpos, flipped=False, suggested=False):
+    def __init__(self, uid, text, masterpos, rank, flipped=False, tag={}, suggested=False):
         self.uid = uid
         self.text = text
-        self.flipped = flipped
-        self.suggested = suggested
         self.masterpos = masterpos
+        self.rank = rank
+        self.flipped = flipped
+        self.tag = tag
+        self.suggested = suggested
+
         # I don't know if these would be helpful or not.
         self.pos = int(self.uid.split(":")[4])
         self.verse = int(self.uid.split(":")[3])
--- api/python_api.py
+++ api/python_api.py
@@ -7,28 +7,57 @@ import mysql.connector as mariadb
 
 
 def get_chapter(chapter_id):
-	pass
+    """
+    To return all chunks for the given chapter in JSON format
+
+    :param chapter_id (str): similar to chunk ID, format: EN:#:#:#:#
+    :return chapter: list of Chunks for the chapter requested
+    """
+    pass
 
 
-def flip_words(not_sure_what_should_go_here):
-	pass
+def flip_words():
+    """
+    Flip all the words for the chapter that have already been flipped.
+    
+    :return flipme (list): list of Chunk uids to flip
+    """
+    pass
 
 
-def get_one_word(position):
-	pass
+def get_one_word(uid):
+    """
+    Get one Chunk from the database.
+
+    :param uid (str): the uid for the Chunk requested.
+    :return chunk (Chunk): The Chunk with the uid specified in the function call.
+    """
+    pass
 
 
-def at_critical_point():
-	pass
+def flip_one_chunk(uid):
+    """
+    Sets one Chunk as flipped in the database.
+
+    :param uid (str): The Chunk uid that needs to be set as flipped.
+    :return:
+    """
+    pass
 
 
-def create_session_table(username):
-	pass
+def set_flipped_list(chunks):
+    """
+    Updates the database with Chunks that were flipped in that session
 
+    :param uid:
+    :return:
+    """
+    pass
 
-def destroy_session_table(username):
-	pass
+def past_critical_point():
+    """
+    Checks if user is past critical point
 
-
-def _url(path):
-    return 'https://example.com' + path
+    :return critical (boolean): True if past critical point, False if not
+    """
+    pass
