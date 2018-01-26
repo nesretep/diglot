2c2dd3c6a588eba6ce8eabdc0a228bbbfc2a8045
Kristian Petersen <nesretep@cs.byu.edu>
2018-01-26 13:30:36 -0700

    Converted Chunk object to an Instance object to match new paradigm

--- api/chunks.py
+++ api/chunks.py
@@ -2,33 +2,30 @@
 # -*- coding: utf-8 -*-
 # Chunk object class definition
 
-class Chunk(object):
+
+class Instance(object):
     """
-        Class object for a chunk of text
+        Class object for a instance of text
 
         Attributes:
             uid (str)         : the unique ID of the chunk in the format LANG:##:##:##:###
             text (str)        : the actual Book of Mormon text of the chunk
             masterpos (str)   : master position this Chunk is connected to
-            flipped (bool)    : False represents not flipped, True represents flipped
-            tag (dict)        : dict holding the boolean value for each of the different tag types
-            rank (int)        : the number representing the difficulty of the Chunk
+            concept_id (str)  : the id of the corresponding concept this Instance is a part of
+            suggested (int)   : 0- not suggested, 1- suggested, 2- suggested, but not flipped
+            
             pos (int)         : the in-verse natural position of the Chunk
             verse (int)       : the verse the Chunk is in
             chapter (int)     : the chapter the Chunk is in
             book (int)        : a numeric representation of the book the Chunk is in with 1 Nephi being 1 and so on
             lang (str)        : the two-letter ISO designation for the language the Chunk is in
-            suggested (int)   : 0- not suggested, 1- suggested, 2- suggested, but not flipped
     """
-    def __init__(self, uid, text, masterpos, rank, flipped, tag, suggested):
+    def __init__(self, uid, text, masterpos, concept_id, suggested):
         self.uid = uid
         self.text = text
         self.masterpos = masterpos
-        self.rank = rank
-        self.flipped = flipped
-        self.tag = tag
+        self.concept_id = concept_id
         self.suggested = suggested
-        # self.cf_id = cf_id
 
         self.pos = int(self.uid.split(":")[4])
         self.verse = int(self.uid.split(":")[3])
