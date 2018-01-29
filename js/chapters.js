da5ff94e8d918d251339935ab23db8657e256863
BraedenQ <braeden.m.quirante@gmail.com>
2018-01-28 20:41:19 -0700

    css edits

---
+++ js/chapters.js
@@ -0,0 +1,23 @@
+var json={
+	"1 Nephi": ["Chapter1","Chapter2","Chapter3","Chapter4","Chapter5","Chapter6","Chapter7","Chapter8","Chapter9","Chapter10","Chapter11","Chapter12","Chapter13","Chapter14","Chapter15","Chapter16","Chapter17","Chapter18","Chapter19","Chapter20","Chapter21","Chapter22"],
+	"2 Nephi": ["Chapter1","Chapter2","Chapter3","Chapter4","Chapter5","Chapter6","Chapter7","Chapter8","Chapter9","Chapter10","Chapter11","Chapter12","Chapter13","Chapter14","Chapter15","Chapter16","Chapter17","Chapter18","Chapter19","Chapter20","Chapter21","Chapter22","Chapter23","Chapter24","Chapter25","Chapter26","Chapter27","Chapter28","Chapter29","Chapter30","Chapter31","Chapter32","Chapter33"],
+	"Jacob": ["Chapter1", "Chapter2", "Chapter3", "Chapter4", "Chapter5","Chapter6","Chapter7"],
+	"Enos": ["Chapter1"],
+	"Jarom": ["Chapter1"],
+	"Omni": ["Chapter1"],
+	"Words of Mormon": ["Chapter1"],
+	"Mosiah": ["Chapter1","Chapter2","Chapter3","Chapter4","Chapter5","Chapter6","Chapter7","Chapter8","Chapter9","Chapter10","Chapter11","Chapter12","Chapter13","Chapter14","Chapter15","Chapter16","Chapter17","Chapter18","Chapter19","Chapter20","Chapter21","Chapter22","Chapter23","Chapter24","Chapter25","Chapter26","Chapter27","Chapter28","Chapter29"],
+	"Alma": ["Chapter1","Chapter2","Chapter3","Chapter4","Chapter5","Chapter6","Chapter7","Chapter8","Chapter9","Chapter10","Chapter11","Chapter12","Chapter13","Chapter14","Chapter15","Chapter16","Chapter17","Chapter18","Chapter19","Chapter20","Chapter21","Chapter22","Chapter23","Chapter24","Chapter25","Chapter26","Chapter27","Chapter28","Chapter29","Chapter30","Chapter31","Chapter32","Chapter33","Chapter34","Chapter35","Chapter36","Chapter37","Chapter38","Chapter39","Chapter40","Chapter41","Chapter42","Chapter43","Chapter44","Chapter45","Chapter46","Chapter47","Chapter48","Chapter49","Chapter50","Chapter51","Chapter52","Chapter53","Chapter54","Chapter55","Chapter56","Chapter57","Chapter58","Chapter59","Chapter60","Chapter61","Chapter62","Chapter63"],
+	"Helaman": ["Chapter1","Chapter2","Chapter3","Chapter4","Chapter5","Chapter6","Chapter7","Chapter8","Chapter9","Chapter10","Chapter11","Chapter12","Chapter13","Chapter14","Chapter15","Chapter16"],
+	"3 Nephi": ["Chapter1","Chapter2","Chapter3","Chapter4","Chapter5","Chapter6","Chapter7","Chapter8","Chapter9","Chapter10","Chapter11","Chapter12","Chapter13","Chapter14","Chapter15","Chapter16","Chapter17","Chapter18","Chapter19","Chapter20","Chapter21","Chapter22","Chapter23","Chapter24","Chapter25","Chapter26","Chapter27","Chapter28","Chapter29","Chapter30"],
+	"4 Nephi": ["Chapter1"],
+	"Mormon": ["Chapter1", "Chapter2", "Chapter3", "Chapter4", "Chapter5","Chapter6","Chapter7","Chapter8","Chapter9"],
+	"Ether": ["Chapter1","Chapter2","Chapter3","Chapter4","Chapter5","Chapter6","Chapter7","Chapter8","Chapter9","Chapter10","Chapter11","Chapter12","Chapter13","Chapter14","Chapter15"],
+	"Moroni": ["Chapter1", "Chapter2", "Chapter3", "Chapter4", "Chapter5","Chapter6","Chapter7","Chapter8","Chapter9","Chapter10"]
+}
+new Vue({
+       el: '#bookChapters',
+       data: {
+           json
+       }
+})
