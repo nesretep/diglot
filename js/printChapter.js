531d316db2416585cc5e74708ed6224769b60285
BraedenQ <braeden.m.quirante@gmail.com>
2018-01-30 21:27:55 -0700

    successful printout of API JSON

--- js/printChapter.js
+++ js/printChapter.js
@@ -837,14 +837,67 @@ markPunctuation();
 getTotalVerses();
 
 var vmprod = new Vue({
-       el: '#chapter',
+       el: '#chapterDep',
        data: {
            json,
            verses
 
        }
 })
+new Vue({
+  el: '#chapter',
+  data: () => ({
+    json: {},
+    verses: []
+  }),
+  created: function() {
+    fetch('http://diglot.it.et.byu.edu/eng/1Nephi/01').then((response) => {
+      return response.json().then((json) => {
+        //console.log("JSON", json)
+        this.json = json
+        //mark punctuation
+        for (i = 0; i < this.json.length; i++) {
+              var position = this.json[i].master_position;
+              position = position.split(":");
+              if(position[0]=="00" && position[1]=="00"){
+                     this.json[i].punctuation = true;
+              }
+              else{
+                     this.json[i].punctuation = false;
+              }
+        }
+        //console.log("JSON", this.json);
+        
+        //get total verses
+        for (i = 0; i < this.json.length; i++) {
+          var position = this.json[i].natural_position;
+          var res = position.split(":");
+          var verse = res[3];
+          verse = parseInt(verse, 10);
+          //alert(verse);
+          //empty array
+          if(this.verses.length == 0){
+              this.verses.push(verse);
+          }
+          //if the last one matches
+          else if(this.verses[this.verses.length-1]== verse){
+              //do nothing
+              //alert(verses[verses.length-1] + " v:" + verse);
+          }
+          //if the last one doesn't match
+          else{
+              this.verses.push(verse);
+          }
+          //append to each chunk object
+          this.json[i].verse = verse;
+         }
+         console.log(this.verses);
+         
 
+      })
+    })
+  }
+})
 function flip_the_phrase3(e){
        //use id to query DB. print result to span        
        var span = $(e).parent().parent();
@@ -994,20 +1047,7 @@ $.ajax(settings).done(function (response) {
   console.log(response);
 });
 */
-new Vue({
-  el: '#app',
-  data: () => ({
-    json: {},
-  }),
-  created: function() {
-    fetch('http://diglot.it.et.byu.edu/eng/1Nephi/01').then((response) => {
-      return response.json().then((json) => {
-        console.log("JSON", json)
-        this.json = json
-      })
-    })
-  }
-})
+
 /*
 var xmlHttp = new XMLHttpRequest();
 xmlHttp.onreadystatechange = function() { 
