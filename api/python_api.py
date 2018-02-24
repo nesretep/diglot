4f014f0670dc435bf3918bc841ce22c07f7891ee
Kristian Petersen <nesretep.naitsirk@gmail.com>
2018-02-24 09:31:25 -0700

    fixed get_all_flipped()

--- api/python_api.py
+++ api/python_api.py
@@ -314,8 +314,9 @@ def get_all_flipped():
     query = "SELECT origin.instance_id, target.instance_id, target.instance_text FROM user_settings AS u \
              INNER JOIN flipped_list AS f on u.user_id = f.user_id INNER JOIN {}_concept AS con ON \
              con.concept_id = f.concept_id INNER JOIN {} origin ON origin.chunk_id = con.chunk_id INNER JOIN {} \
-             AS target ON origin.master_position = target.master_position WHERE user_id = user_id \
+             AS target ON origin.master_position = target.master_position WHERE u.user_id = %s \
              ORDER BY origin.instance_id".format(lang, lang, target_lang)
+    
     try:
         db = helper.connect_to_db(dbconf)
         cursor = db.cursor(mariadb.cursors.DictCursor)
@@ -327,7 +328,7 @@ def get_all_flipped():
     if helper.is_injection(query) == False:
         try:
             cursor.execute(query, (user_id,))
-            query_result = cursor.fetchone()
+            query_result = cursor.fetchall()
             msg = "Query {} executed successfully.".format(query)
             logging.info(msg)
             db.close()
