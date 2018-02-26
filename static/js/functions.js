fbbf7a72f7c50445ab704a5053a950d92a36d83b
BraedenQ <braeden.m.quirante@gmail.com>
2018-02-25 19:17:42 -0700

    clean up code

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,43 +1,25 @@
-//gets json for manipulation
-var chapterJSON = {};
-fetch('http://diglot.it.et.byu.edu/eng/1Nephi/01').then((response) => {
-  return response.json().then((json) => {
-    //console.log("JSON", json)
-     chapterJSON = json;
-  });
-});
-//console.log(chapterJSON);
-
-
-
 function peek(instance_id){
+  //get master position
   var classList = document.getElementById(instance_id).className.split(/\s+/);
   var mp = classList[classList.length-2];
 
-  var position = 0;
-  //iterate through array of JSON to get master position
-  /*for(i = 0; i < chapterJSON.length-1; i++){
-    if(chapterJSON[i].instance_id == instance_id){
-      mp= chapterJSON[i].master_position;
-      position = i;
-      break;
-    }
-  }
-  */
-  //alert(position);
+  //get current language from id
   var chunk = document.getElementById(instance_id);
   var id = $(chunk).attr('id');;
   id = id.split(":");
   var current_lang = id[0];
+
+  //get target language logic
   var lang = "spa";
   if(current_lang == "spa"){
     lang = "eng";
   }
 
+  //fetch
   var url = "http://diglot.it.et.byu.edu/peek?lang=" + lang + "&mp=" + mp;
   fetch(url).then((response) => {
     return response.json().then((json) => {
-      //console.log("JSON", json)
+      //put text in pop up
       var helper = document.getElementById("peek");
       $(helper).text(json["instance_text"]);
       
@@ -45,40 +27,41 @@ function peek(instance_id){
   }); 
 }
 
-function popupVue() {
+function popupVue(){
+  var alreadyPopup = document.getElementsByClassName("popuptext");
+  var exists = $(event.target).children().attr("id");
+  var chunk = $(event.target).attr("id");
 
-       var alreadyPopup = document.getElementsByClassName("popuptext");
-       var exists = $(event.target).children().attr("id");
-       var chunk = $(event.target).attr("id");
-       //alert(chunk);
-       if($(alreadyPopup).attr('id') != null){
-              $(alreadyPopup).remove();
-       }
-       if(chunk == null){
-              //do nothing
-              //this is in case users click on an area that is not a valid chunk
-       }
-       else if(exists == null){
-              var span = $(event.target);
-              span.append("<span class='popuptext' id='myPopup" + chunk + "'></span>");
-              var child = span.children();
-              
-              child.append(" <span id='peek'></span> ");
-              child.append(" <br>"); /*or " | "*/
-              child.append("<span onClick='APIflip(this)'>Flip</span>");
+  //if there is already a pop up, remove it
+  if($(alreadyPopup).attr('id') != null){
+    $(alreadyPopup).remove();
+  }
+
+  //pop up logic
+  if(chunk == null){
+    //do nothing
+    //this is in case users click on an area that is not a valid chunk
+  }
+  else if(exists == null){
+    var span = $(event.target);
+    span.append("<span class='popuptext' id='myPopup" + chunk + "'></span>");
+    var child = span.children();
+
+    child.append(" <span id='peek'></span> ");
+    child.append(" <br>"); /*or " | "*/
+    child.append("<span onClick='APIflip(this)'>Flip</span>");
 
 
-              var popupChunk = "myPopup" + chunk;
-              var popup = document.getElementById(popupChunk);
-              popup.classList.toggle("show");
-              peek(chunk);
-       }
-       else{
-              var span = $(event.target);
-              span.children().remove();
-       }
+    var popupChunk = "myPopup" + chunk;
+    var popup = document.getElementById(popupChunk);
+    popup.classList.toggle("show");
+    peek(chunk);
+  }
+  else{
+    var span = $(event.target);
+    span.children().remove();
+  }
 }
-
 
 function APIflip_back(e){
 
@@ -135,8 +118,8 @@ function APIflip_back(e){
     });
   });
   //change tags
-
 }
+
 function APIflip(e){
   //get the instance id
     
