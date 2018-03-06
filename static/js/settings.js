5fb6ec17c9bf797f204ba5004ba7209144b9d602
BraedenQ <braeden.m.quirante@gmail.com>
2018-03-05 21:49:57 -0700

    css of settings modal

--- static/js/settings.js
+++ static/js/settings.js
@@ -1,6 +1,13 @@
 $("#difficulty").val(sessionStorage.getItem("level"));
 $("#rate").val(sessionStorage.getItem("rate"));
 
+var source_lang = sessionStorage.getItem("origin_lang_id");
+var target_lang = sessionStorage.getItem("target_lang_id");
+
+$("#source_langs").children("#source-" + source_lang).addClass("active");
+$("#target_langs").children("#target-" + target_lang).addClass("active");
+
+//buttons
 $("#difficulty-button-increment").click(function(){
     var level = $("#difficulty").val();
     level = parseInt(level);
@@ -26,3 +33,6 @@ $("#rate-button-decrement").click(function(){
     rate--;
     $("#rate").val(rate);
 });
+
+
+
