0a6105d724d40b305f5c0673c9b1d5075150dbf2
Kristian Petersen <nesretep@cs.byu.edu>
2018-03-07 10:39:48 -0700

    included some TODOs in the code

--- api/recommend.py
+++ api/recommend.py
@@ -6,20 +6,25 @@ import sys
 import pymysql as mariadb
 import logging
 
+# TODO: this needs to be inside the function
 db = helper.connect_to_db(dbconf)
 cursor = db.cursor(mariadb.cursors.DictCursor)
 
 @bottle.route('/recommender')
 def recommend:
-
+    # TODO: write a docstring outlining your parameters (what the front end will send you) and what this function returns
+    # TODO: implement logging in your code to log both successes and failures to the log file
 	query1 = "SELECT user_id, `level`, rate FROM user_info"
 	query2 = "SELECT score FROM (lang)_concept_data WHERE `level`.user_info == score"
 	
-
+    # TODO: write lines to retrieve the data you need from the query string (not from SQL queries)
 	fetchlevel = bottle.request.query.level
 
+    # TODO: fix the try/except blocks; they need an except for each try so they can gracefully handle the failure
 	try:
 		cursor.execute(query1)#Run query1
+        # TODO: you need to actually fetch the data from the cursor and store it before you can manipulate it
+        # Use cursor.fetchone() or cursor.fetchall()
 		query1_result = level + 1 #Add 1 to the user level
 		return json.dumps(query_result1) #JSON-ify the result from query
 		level = query_result1 #updates user score
