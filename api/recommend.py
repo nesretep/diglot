d32ee741829636f37cd6d2dce34b82e481cace73
ThunderPine <32583442+ThunderPine@users.noreply.github.com>
2018-03-07 08:29:01 -0700

    Update recommend.py

--- api/recommend.py
+++ api/recommend.py
@@ -12,33 +12,27 @@ cursor = db.cursor(mariadb.cursors.DictCursor)
 @bottle.route('/recommender')
 def recommend:
 
-		
-		query = "SELECT level, rate FROM user_info"
-		query2 = "SELECT score FROM (lang)_concept_data WHERE level.user_info == score"
+	query1 = "SELECT level, rate FROM user_info"
+	query2 = "SELECT score FROM (lang)_concept_data WHERE level.user_info == score"
 	
 	if helper.check_login = True #This helper thing to call in the user login info
 		level = bottle.request.query.level
 
 		try:
-			cursor.execute(query2) #Run query2
-			cursor.close()
-			db.close()
-
-		try:
-			cursor.execute(query)#Run query1
+			cursor.execute(query1)#Run query1
 			query1_result = level + 1 #Add 1 to the user level
-			return json.dumps(query_result) #JSON-ify the result from query
-			level = query_result #updates user score
-			for query_result #For that updated score
+			return json.dumps(query_result1) #JSON-ify the result from query
+			level = query_result1 #updates user score
+			for query1_result #For that updated score
 				cursor.execute(query2) #Query the db for the words where the new user score = level
 			cursor.close()
 			db.close()
 
 		try:
-			cursor.execute(query)
+			cursor.execute(query1)
 			#***Send request for the number of recommended words***
-			query_result = score 
-			return json.dumps(query_result)
+			query1_result = score 
+			return json.dumps(query1_result)
 		#***Send the queried data back to the front end for which words to recommend***
 			cursor.close()
 			db.close()
